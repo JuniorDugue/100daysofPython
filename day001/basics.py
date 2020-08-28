345bd5e4bca542802a2b43ca46eae9f6c4ba156c
juniordugue <jrdugue@gmail.com>
2020-08-27 23:52:56 -0400

    day002 went over variables, strings, integers and floats

--- day001/basics.py
+++ day001/basics.py
@@ -1,6 +1,55 @@
+# this will print the date and time
+# import datetime
+# print (datetime.datetime.now())
+
+
+# this will print a string with the date & time on 2 different lines
 # import datetime
 # print("The date and time is")
 # print (datetime.datetime.now())
 
+
+# this will print everything on one line
+# import datetime
+# print("The date and time is", datetime.datetime.now())
+
+
+# variables
 import datetime
-print("The date and time is", datetime.datetime.now())
+mynow = datetime.datetime.now()
+print(mynow)
+
+# different types of variables we can have
+mynumber = 10
+mytext = "Hello"
+
+# to print the variables we created above
+print(mynumber, mytext)
+
+# different types of variables we cannot use
+# -
+# numbers 
+
+
+
+#simple types: integers, strings and floats
+x = 10
+y = "10"
+
+sum1 = x + x
+sum2 = y + y
+print(sum1, sum2)
+
+# int is short integer
+# str is short for string
+# int x = 10 ---> x = 10
+# str y = 10 ---> y = "10"
+
+sum1 = x + x
+sum2 = y + y
+print(sum1, sum2)
+
+
+
+z = 10.1
+print(type(x), type(y), type(z))
---
+++ day001/quiz1.py
@@ -0,0 +1,7 @@
+# assign some values to variables "x" "y" and "z" then insert x, y, and z inside a print() function to print out the values of those variables
+
+x = "python"
+y = "is"
+z = "fun"
+
+print(x, y, z)
